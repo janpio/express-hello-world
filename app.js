cb594dea6ddf16e5271e84c423b59900251da846
Jan Piotrowski <piotrowski+github@gmail.com>
2022-01-24 23:12:03 +0000

    add memory too app.js as well

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 app.get("/", async (req, res) => {
+
+    console.log("============")
+    for (const [key,value] of Object.entries(process.memoryUsage())){
+        console.log(`Memory usage by ${key}, ${value/1000000}MB `)
+    }
+    console.log("============")
+
     const contacts = await prisma.contact.findMany()
     res.send(`contacts = ${contacts.length}`)
 });
