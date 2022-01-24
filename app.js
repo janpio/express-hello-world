bd26636918ae6ec1ec0437d97ccdda68c9c83445
Jan Piotrowski <piotrowski+github@gmail.com>
2022-01-24 22:41:42 +0000

    add Prisma

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send("Hello there!"));
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/", async (req, res) => {
+    const contacts = await prisma.contact.findMany()
+    res.send(`contacts = ${contacts.length}`)
+});
+
+app.listen(port, async () => {
+    console.log(`Example app listening on port ${port}!`)
+});
